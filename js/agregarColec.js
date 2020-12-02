a549fa1e251a001a8abea010e2cdd82abc8c8e56
Roger Retana Mc Nally <rogerretanamc@gmail.com>
2020-12-01 23:48:42 -0600

    Primera construcción del front end

---
+++ js/agregarColec.js
@@ -0,0 +1,33 @@
+'use strict'
+
+function sendNewTable() {
+
+    var name = document.getElementById("coleccion").value;
+
+    if (name) {
+
+        var params = {
+            name:name
+        }
+
+        fetch('https://nu3t44r9jf.execute-api.us-east-2.amazonaws.com/prod/createcol', {
+            method:'POST',
+            body: JSON.stringify(params),
+            headers: {'Content-Type':'application/json'},
+            mode: "cors"
+        })
+            .then(data => {
+                if (data) {
+                    alert("Se ha creado una nueva tabla");
+                } else {
+                    alert("No se ha podido crear una nueva tabla");
+                }
+            });
+
+    } else {
+        alert("Por favor primero llene el espacio en blanco y dele enviar");
+    }
+
+
+
+}
